9c2b69ddd75dd61fc56f080c926d6fdb05fbecd5
Damish <damishs88@gmail.com>
2022-02-08 13:39:04 +0530

    initial commit

---
+++ src/main/java/com/example/jenkinstestproj/JenkinsTestProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinstestproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsTestProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jenkinstestproj/JenkinsTestProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinstestproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
